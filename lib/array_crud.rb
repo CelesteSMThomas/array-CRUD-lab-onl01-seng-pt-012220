879960da90912ab42ce4212669d8bd8c0df282eb
Celeste Thomas <celestesmthomas@gmail.com>
2020-01-29 17:03:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,15 +3,16 @@ def create_an_empty_array
 end
 
 def create_an_array
-  
+  [1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
+  add_element_to_end_of_array.push(5)
   
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  add_element_to_start_of_array.unshift(0)
 end
 
 def remove_element_from_end_of_array(array)
